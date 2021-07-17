5292a9ee070aacedeb3d544cbdb20cd7f4f16f5e
Noma <wiostz98kr@gmail.com>
2021-07-17 09:52:56 +0900

    F_Feat: Add login page

--- client/src/components/views/loginPage/loginPage.jsx
+++ client/src/components/views/loginPage/loginPage.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+import { useHistory } from 'react-router-dom';
 
 function LoginPage() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const body = {
+            email,
+            password,
+        }
+        dispatch(loginUser(body))
+            .then(response => {
+                if (response.payload.loginSuccess) {
+                    history.push("/");
+                } else {
+                    alert("⛔로그인 실패");
+                }
+            })
+
+    }
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
+        }}>
+            <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
+                <label>Email</label>
+                <input type="email" value={email} onChange={handleEmail} />
+                <label>Password</label>
+                <input type="password" value={password} onChange={handlePassword} />
+                <br />
+                <button>Login</button>
+            </form>
         </div>
     )
 }
